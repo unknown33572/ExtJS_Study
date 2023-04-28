14401fe72797f09746b4714efde2ab709f4ed011
Your Name <unknown33572@gmail.com>
2023-04-28 15:00:05 +0900

    third commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 Ext.onReady(function() {
     Ext.Msg.alert('Hello', 'World!');
     Ext.create("Ext.panel.Panel", {
-        width : 300,
-        height : 200,
+        width : 500,
+        height : 300,
+        padding : 55,
         renderTo : Ext.getBody(),
         border : true,
         items : [{
@@ -16,16 +17,44 @@ Ext.onReady(function() {
     });
     Ext.create("Ext.container.Viewport", {
         renderTo : Ext.getBody(),
-        layout : "border",
+        layout : 'border',
         border : true,
         items : [{
             xtype : 'panel',
+            height : 300,
+            flex : 1,
+            border : true,
             region : 'north',
             title : '패널',
+            items : [{
+                xtype : 'panel',
+                border : true,
+                flex : 1,
+                region : 'west',
+            }, {
+                xtype : 'panel',
+                border : true,
+                flex : 1,
+                region : 'east',
+            }]
         }, {
             xtype : 'panel',
-            region : 'center',
+            flex : 1,
+            border : true,
+            region : 'west',
             title : '패널2',
+        }, {
+            xtype : 'panel',
+            flex : 1,
+            border : true,
+            region : 'east',
+            title : '패널3',
+        }, {
+            xtype : 'panel',
+            flex : 1,
+            border : true,
+            region : 'south',
+            title : '패널4',
         }]
     });
 });
