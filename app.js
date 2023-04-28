0695ba3f84c4e8e6cb5c1a3066dc01be4ab965c4
Your Name <unknown33572@gmail.com>
2023-04-28 14:03:47 +0900

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 Ext.onReady(function() {
     Ext.Msg.alert('Hello', 'World!');
+    Ext.create("Ext.panel.Panel", {
+        width : 300,
+        height : 200,
+        renderTo : Ext.getBody(),
+        border : true,
+        items : [{
+            xtype : 'numberfield',
+        }, {
+            xtype : 'textfield',
+        }, {
+            xtype : 'button',
+            text : 'Click Me!'
+        }]
+    });
+    Ext.create("Ext.container.Viewport", {
+        renderTo : Ext.getBody(),
+        layout : "border",
+        border : true,
+        items : [{
+            xtype : 'panel',
+            region : 'north',
+            title : '패널',
+        }, {
+            xtype : 'panel',
+            region : 'center',
+            title : '패널2',
+        }]
+    });
 });
